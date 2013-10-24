d7da019c12212d0822ea349c21472085492152b6
Carlos Soares <cmpsoares91@gmail.com>
2013-10-24 12:30:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,15 @@
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
+sudo apt-get install -y git-core
 sudo apt-get install -y git
 sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+nvm install v0.10.21
+nvm use v0.10.21
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/

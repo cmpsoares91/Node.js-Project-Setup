fba0122542a7d4346fa65e62b1c0254be1674335
Carlos Soares <cmpsoares91@gmail.com>
2013-10-25 02:14:19 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+# 
+
+if [ $1 == server ] then
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+    wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# All extra stuff for server add here
+
+    echo "Server Setup Ready"
+fi
+if [ $1 == dev ] then
+    echo "Developer Setup Ready"
+else
+    echo "No Specific selectio made."
+fi

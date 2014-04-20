eef8d03f19b579e8096dc60e90b975a1ab1baf57
Carlos Soares <cmpsoares91@gmail.com>
2014-04-20 19:35:31 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
 #Bash Script for installation testing
+
+#Need to add commands to test it...
+for cmd in "latex" "pandoc"; do
+  printf "%-10s" "$cmd"
+  if hash "$cmd" 2>/dev/null; then printf "OK\n"; else printf "missing\n"; fi
+done

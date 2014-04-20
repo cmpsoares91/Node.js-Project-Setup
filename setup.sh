adfe8060fda5fe75d4144a2a2f3eb12e7e9b7810
Carlos Soares <cmpsoares91@gmail.com>
2014-04-20 19:01:25 +0100

    Making setup.sh independent from setup-base.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,57 @@
 #!/bin/bash
 
-# Simple setup.sh for ISMAT School Project
-cd
-apt-get install -y curl
+function baseInstall() {
+	# Simple setup.sh for ISMAT School Project
+	cd
+	apt-get install -y curl
+	
+	echo "Installing project structure:"
+
+	# Installing dependencies:
+	sudo apt-get update
+	sudo apt-get install -y git
+	sudo apt-get install -y gcc g++ autoconf automake libtool
+	sudo apt-get install -y python-software-properties python make
+	sudo apt-get install -y build-essential libssl-dev
+
+	# Install jshint to allow checking of JS code within emacs
+	# http://jshint.com/
+	npm install -g jshint
+
+	# Install rlwrap to provide libreadline features with node
+	# See: http://nodejs.org/api/repl.html#repl_repl
+	sudo apt-get install -y rlwrap
+
+	# Install emacs24
+	# https://launchpad.net/~cassou/+archive/emacs
+	sudo add-apt-repository -y ppa:cassou/emacs
+	sudo apt-get -qq update
+	sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
+	# Install nvm: node-version manager
+	# https://github.com/creationix/nvm
+	curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+	# Load nvm and install latest production node
+	source $HOME/.nvm/nvm.sh
+	nvm install v0.10
+	nvm use v0.10
+
+	# git pull and install dotfiles as well
+	cd $HOME
+	if [ -d ./dotfiles/ ]; then
+		mv dotfiles dotfiles.old
+	fi
+	if [ -d .emacs.d/ ]; then
+    		mv .emacs.d .emacs.d~
+	fi
+	git clone https://github.com/cmpsoares91/dotfiles.git
+	ln -sb dotfiles/.screenrc .
+	ln -sb dotfiles/.bash_profile .
+	ln -sb dotfiles/.bashrc .
+	ln -sb dotfiles/.bashrc_custom .
+	ln -sf dotfiles/.emacs.d .
+}
 
 install=1
 
@@ -10,8 +59,8 @@ if [ $1 == server ] ; then
     echo "Starting Server Instalation..."
 
     # Instaling Base:
-    curl https://raw.github.com/cmpsoares91/Node.js-Project-Setup/master/setup-base.sh | sh
-
+    baseInstall
+    
     # Install Heroku toolbelt
     # https://toolbelt.heroku.com/debian
     wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
@@ -24,7 +73,7 @@ elif [ $1 == dev ] ; then
     echo "Starting Developer Instalation..."
 
     # Instaling Base:
-    curl https://raw.github.com/cmpsoares91/Node.js-Project-Setup/master/setup-base.sh | sh
+    baseInstall
 
     echo "Developer Setup Ready"
  
@@ -79,7 +128,7 @@ if [ install == 1 ] ; then
             done
         elif [ $meanOption == n ] ; then
             #Nothing Happens...
-            echo "Not installing mean..."
+            echo "Not installing MEAN..."
         else
             echo "Please enter y or n:"
             a=1

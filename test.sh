e83749be97418abc3da35d41676eeb9f333c9440
Carlos Soares <cmpsoares91@gmail.com>
2014-04-21 02:06:53 +0100

    removing some non-executables
    need to find out what node, nvm, npm, mean, bower, and grunt are and how to test it...

--- test.sh
+++ test.sh
@@ -2,24 +2,24 @@
 
 #Need to add commands to test it...
 echo "Testing Basic dependencies:"
-for cmd in "git" "gcc" "g++" "autoconf" "automake" "libtool" "python-software-properties" "python" "make" "build-essential" "libssl-dev"; do
+for cmd in "git" "gcc" "g++" "autoconf" "automake" "libtool" "python" "make" ; do
   printf "%-30s" "$cmd"
   if hash "$cmd" 2>/dev/null; then printf "OK\n"; else printf "missing\n"; fi
 done
 
-echo "Testing Basic command line editor installations:"
-for cmd in "jshint" "rlwrap" "emacs" "nano"; do
+echo -e "\nTesting Basic command line editor installations:"
+for cmd in "rlwrap" "emacs" "nano"; do
   printf "%-30s" "$cmd"
   if hash "$cmd" 2>/dev/null; then printf "OK\n"; else printf "missing\n"; fi
 done
 
-echo "Testing Node.JS installations:"
+echo -e "\nTesting Node.JS installations:"
 for cmd in "node" "nvm" "npm" "heroku" "foreman"; do
   printf "%-30s" "$cmd"
   if hash "$cmd" 2>/dev/null; then printf "OK\n"; else printf "missing\n"; fi
 done
 
-echo "Testing MEAN JS Stack installations:"
+echo -e "\nTesting MEAN JS Stack installations:"
 for cmd in "mongo" "mongod" "mean" "bower" "grunt"; do
   printf "%-30s" "$cmd"
   if hash "$cmd" 2>/dev/null; then printf "OK\n"; else printf "missing\n"; fi

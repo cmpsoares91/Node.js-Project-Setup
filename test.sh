5d6a642a0bbb099078030151b50d4633456323a5
Carlos Soares <cmpsoares91@gmail.com>
2014-04-21 12:28:50 +0100

    identation

--- test.sh
+++ test.sh
@@ -6,23 +6,39 @@
 echo "Testing Basic dependencies:"
 for cmd in "git" "gcc" "g++" "autoconf" "automake" "libtool" "python" "make" ; do
   printf "%-30s" "$cmd"
-  if hash "$cmd" 2>/dev/null; then printf "OK\n"; else printf "missing\n"; fi
+  if hash "$cmd" 2>/dev/null; then 
+    printf "OK\n";
+  else
+    printf "missing\n"; 
+  fi
 done
 
 echo -e "\nTesting Basic command line editor installations:"
 for cmd in "rlwrap" "emacs" "nano"; do
   printf "%-30s" "$cmd"
-  if hash "$cmd" 2>/dev/null; then printf "OK\n"; else printf "missing\n"; fi
+  if hash "$cmd" 2>/dev/null; then
+    printf "OK\n";
+  else
+    printf "missing\n";
+  fi
 done
 
 echo -e "\nTesting Node.JS installations:"
 for cmd in "node" "nvm" "npm" "heroku" "foreman"; do
   printf "%-30s" "$cmd"
-  if hash "$cmd" 2>/dev/null; then printf "OK\n"; else printf "missing\n"; fi
+  if hash "$cmd" 2>/dev/null; then
+    printf "OK\n";
+  else
+    printf "missing\n";
+  fi
 done
 
 echo -e "\nTesting MEAN JS Stack installations:"
 for cmd in "mongo" "mongod" "mean" "bower" "grunt"; do
   printf "%-30s" "$cmd"
-  if hash "$cmd" 2>/dev/null; then printf "OK\n"; else printf "missing\n"; fi
+  if hash "$cmd" 2>/dev/null; then
+    printf "OK\n";
+  else
+    printf "missing\n";
+  fi
 done

f5c959e28565f353aa49ea0e86aa7d972e1466ae
Carlos Soares <cmpsoares91@gmail.com>
2014-04-21 13:24:27 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -59,11 +59,17 @@ function baseInstall() {
 	curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 	# Load nvm and install latest production node
-	source $HOME/.nvm/nvm.sh
-	source ~/.profile
-	nvm ls-remote
-	nvm install v0.10.26
-	nvm use v0.10.26
+	#source $HOME/.nvm/nvm.sh
+	#source ~/.profile
+	#nvm ls-remote
+	
+	#Trying the manual installation approach:
+	git clone https://github.com/creationix/nvm.git ~/.nvm
+	source ~/.nvm/nvm.sh
+	
+	#Installing last node version
+	nvm install v0.10
+	nvm use v0.10
 
 	# git pull and install dotfiles as well
 	cd $HOME
